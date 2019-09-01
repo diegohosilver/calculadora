48a38f9dcaf6fe88eac29675d5c3ed36d93afd06
Diego Silvera de Souza <ctk.591@gmail.com>
2019-09-01 13:29:48 -0300

    Se agregan tests para clase calculo.

--- src/test/calculo/CalculoTest.java
+++ src/test/calculo/CalculoTest.java
@@ -1,13 +1,32 @@
 package test.calculo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import main.negocio.Calculo;
+import main.negocio.Operacion;
 
 public class CalculoTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void CalculoSinOperacionTest() {
 		Calculo calculo = new Calculo(null);
+	}
+	
+	@Test
+	public void ObtenerOperadorTest() {
+		Calculo calculo = new Calculo(Operacion.SUMA);
+		
+		assertTrue(calculo.obtenerOperadorActual() == Operacion.SUMA);
+	}
+	
+	@Test
+	public void CambiarOperadorTest() {
+		Calculo calculo = new Calculo(Operacion.SUMA);
+		
+		calculo.cambiarOperador(Operacion.RESTA);
+		
+		assertTrue(calculo.obtenerOperadorActual() == Operacion.RESTA);
 	}
 }
