fa545b7b7cea8ffbbf28376fe466cc6b8cef4cf3
alumnos <alumnos@7118-16>
2019-09-03 20:45:50 -0300

    Modificacion de los nombres en los botones de la interfaz

--- src/main/interfaz/PantallaPrincipal.java
+++ src/main/interfaz/PantallaPrincipal.java
@@ -10,6 +10,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
 public class PantallaPrincipal {
 
@@ -44,131 +47,97 @@ public class PantallaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 540, 640);
+		frame.setBounds(100, 100, 525, 640);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(10, 11, 512, 176);
+		textField.setBackground(Color.WHITE);
+		textField.setEditable(false);
+		textField.setFont(textField.getFont().deriveFont(textField.getFont().getStyle() | Font.BOLD, textField.getFont().getSize() + 9f));
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField.setBounds(10, 11, 494, 176);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton.setBounds(10, 198, 116, 76);
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton button = new JButton("New button");
-		button.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button.setBounds(136, 198, 116, 76);
+		JButton button = new JButton("Borrar");
+		button.setFont(button.getFont().deriveFont(button.getFont().getStyle() | Font.BOLD, 20f));
+		button.setBounds(10, 198, 116, 76);
 		frame.getContentPane().add(button);
 		
-		JButton button_1 = new JButton("New button");
-		button_1.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_1.setBounds(262, 198, 116, 76);
+		JButton button_1 = new JButton("+");
+		button_1.setFont(button_1.getFont().deriveFont(button_1.getFont().getStyle() | Font.BOLD, 20f));
+		button_1.setBounds(136, 198, 116, 76);
 		frame.getContentPane().add(button_1);
 		
-		JButton button_2 = new JButton("New button");
-		button_2.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
+		JButton button_2 = new JButton("7");
+		button_2.setFont(button_2.getFont().deriveFont(button_2.getFont().getStyle() | Font.BOLD, 20f));
 		button_2.setBounds(10, 290, 116, 76);
 		frame.getContentPane().add(button_2);
 		
-		JButton button_3 = new JButton("New button");
-		button_3.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
+		JButton button_3 = new JButton("8");
+		button_3.setFont(button_3.getFont().deriveFont(button_3.getFont().getStyle() | Font.BOLD, 20f));
 		button_3.setBounds(136, 290, 116, 76);
 		frame.getContentPane().add(button_3);
 		
-		JButton button_4 = new JButton("New button");
-		button_4.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_4.setBounds(262, 290, 116, 76);
+		JButton button_4 = new JButton("-");
+		button_4.setFont(button_4.getFont().deriveFont(button_4.getFont().getStyle() | Font.BOLD, 20f));
+		button_4.setBounds(262, 198, 116, 76);
 		frame.getContentPane().add(button_4);
 		
-		JButton button_5 = new JButton("New button");
-		button_5.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_5.setBounds(10, 377, 116, 76);
+		JButton button_5 = new JButton("*");
+		button_5.setFont(button_5.getFont().deriveFont(button_5.getFont().getStyle() | Font.BOLD, 20f));
+		button_5.setBounds(388, 198, 116, 76);
 		frame.getContentPane().add(button_5);
 		
-		JButton button_6 = new JButton("New button");
-		button_6.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_6.setBounds(136, 377, 116, 76);
+		JButton button_6 = new JButton("9");
+		button_6.setFont(button_6.getFont().deriveFont(button_6.getFont().getStyle() | Font.BOLD, 20f));
+		button_6.setBounds(262, 290, 116, 76);
 		frame.getContentPane().add(button_6);
 		
-		JButton button_7 = new JButton("New button");
-		button_7.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_7.setBounds(262, 377, 116, 76);
+		JButton button_7 = new JButton("/");
+		button_7.setFont(button_7.getFont().deriveFont(button_7.getFont().getStyle() | Font.BOLD, 20f));
+		button_7.setBounds(388, 290, 116, 76);
 		frame.getContentPane().add(button_7);
 		
-		JButton button_8 = new JButton("New button");
-		button_8.setBounds(10, 464, 116, 76);
+		JButton button_8 = new JButton("INTRO");
+		button_8.setFont(button_8.getFont().deriveFont(button_8.getFont().getStyle() | Font.BOLD, 20f));
+		button_8.setBounds(388, 377, 116, 76);
 		frame.getContentPane().add(button_8);
 		
-		JButton button_9 = new JButton("New button");
-		button_9.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
-		});
-		button_9.setBounds(136, 464, 116, 76);
+		JButton button_9 = new JButton("6");
+		button_9.setFont(button_9.getFont().deriveFont(button_9.getFont().getStyle() | Font.BOLD, 20f));
+		button_9.setBounds(262, 377, 116, 76);
 		frame.getContentPane().add(button_9);
 		
-		JButton button_10 = new JButton("New button");
-		button_10.setBounds(262, 464, 116, 76);
+		JButton button_10 = new JButton("5");
+		button_10.setFont(button_10.getFont().deriveFont(button_10.getFont().getStyle() | Font.BOLD, 20f));
+		button_10.setBounds(136, 377, 116, 76);
 		frame.getContentPane().add(button_10);
 		
-		JButton button_11 = new JButton("New button");
-		button_11.setBounds(388, 198, 116, 76);
+		JButton button_11 = new JButton("4");
+		button_11.setFont(button_11.getFont().deriveFont(button_11.getFont().getStyle() | Font.BOLD, 20f));
+		button_11.setBounds(10, 377, 116, 76);
 		frame.getContentPane().add(button_11);
 		
-		JButton button_12 = new JButton("New button");
-		button_12.setBounds(388, 290, 116, 76);
+		JButton button_12 = new JButton("1");
+		button_12.setFont(button_12.getFont().deriveFont(button_12.getFont().getStyle() | Font.BOLD, 20f));
+		button_12.setBounds(10, 464, 116, 76);
 		frame.getContentPane().add(button_12);
 		
-		JButton button_13 = new JButton("New button");
-		button_13.setBounds(388, 377, 116, 76);
+		JButton button_13 = new JButton("2");
+		button_13.setFont(button_13.getFont().deriveFont(button_13.getFont().getStyle() | Font.BOLD, 20f));
+		button_13.setBounds(136, 464, 116, 76);
 		frame.getContentPane().add(button_13);
 		
-		JButton button_14 = new JButton("New button");
-		button_14.setBounds(388, 464, 116, 76);
+		JButton button_14 = new JButton("3");
+		button_14.setFont(button_14.getFont().deriveFont(button_14.getFont().getStyle() | Font.BOLD, 20f));
+		button_14.setBounds(262, 464, 116, 76);
 		frame.getContentPane().add(button_14);
+		
+		JButton button_15 = new JButton("0");
+		button_15.setFont(button_15.getFont().deriveFont(button_15.getFont().getStyle() | Font.BOLD, 20f));
+		button_15.setBounds(388, 464, 116, 76);
+		frame.getContentPane().add(button_15);
 	}
 }
